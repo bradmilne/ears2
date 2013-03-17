968b9187632afd8626947d8b7247d3c083b7e27f
Brad Milne <brad.milne@getelevate.com>
2013-03-17 17:54:46 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ears2::Application.config.secret_token = '5130a151df59fd48472dae427f82d1cec7940c288d0d6a7d34410bea2d7c8f63a734117504eb23ec5894c1c1de3dd7cc3f55e4ac167268d9a15ae22e9c377d0f'
