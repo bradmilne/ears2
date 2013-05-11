961bad0e2f6bb9360ddd0adce6644dbf8495aa15
Brad Milne <brad.milne@getelevate.com>
2013-05-11 13:48:49 -0400

    small changes to lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,16 +3,10 @@ class LessonsController < ApplicationController
   	@lesson = Lesson.find(params[:id])
   	@questions = Question.where(:lesson_id => params[:id])
   	@questions.each do |question|
+     #consider changing this so it's per response type, not question
   	  total_answers = Response.where(:user_id => current_user.id, :question_id => question.id).count
   	  total_correct = Response.where(:user_id => current_user.id, :question_id => question.id, :result => "true").count
   	  question[:avg_score] = "#{total_correct}/#{total_answers}"
-  	  #if Response.where(:user_id => current_user.id, :question_id => question.id).count < 1
-        #question[:avg_score] = Response.where(:user_id => current_user.id, :question_id => question.id).count
-      #else
-      	#correct_answers = Response.where(:user_id => current_user.id, :question_id => question.id, :result => true).count
-      	#total_answers    = Response.where(:user_id => current_user.id, :question_id => question.id).count
-        #question[:avg_score] = (correct_answers/total_answers)*100.to_i
-      #end
      end
   end
 end
